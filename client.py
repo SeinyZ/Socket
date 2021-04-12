7a6bb7a9d2bd657d1bfee83edd6abf227360e78f
SeinyZ <69090478+SeinyZ@users.noreply.github.com>
2021-04-12 22:30:28 +0200

    Prova

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+
 import socket
 
 SERVER_ADDRESS = '127.0.0.1'
@@ -7,33 +8,36 @@ sock_service = socket.socket()
 
 sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
 
-print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
-while True:
-    try:
-        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ")
-    except EOFError:
-        print("\nOkay. Exit")
-        break
-    if not dati:
-        print("Non puoi inviare una stringa vuota!")
-        continue
-    if dati == '0':
-        print("Chiudo la connessione con il server!")
-        break
-    
-    dati = dati.encode()
+print("Client connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
 
+modello=["SYN", "SYN ACK", "ACK with Data", "ACK for Data"]
+step=0
+dati = str(step)
+
+while True:
+    dati=dati.encode() 
     sock_service.send(dati)
+    print("Inviato:" + str(step) + " - " + modello[step])
+    
 
     dati = sock_service.recv(2048)
 
     if not dati:
         print("Server non risponde. Exit")
         break
-    
-    dati = dati.decode()
 
-    print("Ricevuto dal server:")
-    print(dati + '\n')
+    dati = dati.decode()
+    step = int(dati)
+
+    if dati == '3':
+        print("Ricevuto:" + str(step) + " - " + modello[step])
+        print("Termino la connessione con il server.")
+        break
+
+    else:
+        step = int(dati)
+        print("Ricevuto:" + str(step) + " - " + modello[step])
+        step+=1
+        dati= str(step)
 
 sock_service.close()
